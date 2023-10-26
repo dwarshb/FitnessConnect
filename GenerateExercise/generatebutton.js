96d2adee28998e64032942ff67dc4ef15799557f
Pratiq999 <101226028+Pratiq999@users.noreply.github.com>
2023-10-26 11:03:35 -0400

    Create generatebutton.js

---
+++ GenerateExercise/generatebutton.js
@@ -0,0 +1,9 @@
+function GenerateButton() {
+    return (
+        <button className="generate-button">
+            Generate Exercise
+        </button>
+    );
+}
+
+export default GenerateButton;
