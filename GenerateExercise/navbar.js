a97c42104ff9c9a6a8075266da192f3157fe7fee
Pratiq999 <101226028+Pratiq999@users.noreply.github.com>
2023-10-26 11:04:22 -0400

    Create navbar.js

---
+++ GenerateExercise/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Frame } from "./components/Frame";  // Adjusted import path
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <Frame />
+    </div>
+  );
+}
+
+export default Navbar;
