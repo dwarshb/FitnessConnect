2cf46d73e210c32bb241d28fc10c25e8842e1f5d
Pratiq999 <101226028+Pratiq999@users.noreply.github.com>
2023-10-26 11:03:15 -0400

    Create frame.js

---
+++ GenerateExercise/frame.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Frame.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="text-wrapper">BACK</div>
+    </div>
+  );
+};
