4ac61ebb7f8dd4552c927fa2f972dc484f95ea45
Pratiq999 <101226028+Pratiq999@users.noreply.github.com>
2023-10-26 11:04:05 -0400

    Create header.js

---
+++ GenerateExercise/header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <div className="header">
+            <img src="C:\Users\prati\OneDrive\Desktop\SPRINT 2\gen_ex\public\logo-removebg-preview.png"
+            alt="Fitness Connect" 
+            id="LogoremovebgpreviewRoot"
+            className="w-min" />
+        </div>
+    );
+}
+
+export default Header;
