5126b2efe9f432a0f11e46ed9979b2e297456276
Pratiq999 <101226028+Pratiq999@users.noreply.github.com>
2023-10-26 10:57:35 -0400

    Create description.js

---
+++ GenerateExercise/src/components/description.js
@@ -0,0 +1,11 @@
+function Description() {
+    return (
+        <div 
+        id="SubtitleRoot"
+        className="text-center text-4xl font-sans leading-[50.6px] w-full">
+            Press Generate Exercise, to get started with your journey of fitness with an array of simple exercises.
+        </div>
+    );
+}
+
+export default Description;
